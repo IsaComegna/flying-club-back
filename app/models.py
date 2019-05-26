0f3d38bdbdea9023c4a963ab839b36b6aafb3e06
Isabela Comegna <isacomegna@gmail.com>
2019-05-26 20:11:08 -0300

    Change models attributes

--- app/models.py
+++ app/models.py
@@ -6,10 +6,12 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True)
   email = db.Column(db.String(120), unique=True)
+  user_type = db.Column(db.String)
 
-  def __init__(self, username, email):
+  def __init__(self, username, email, user_type):
     self.username = username
     self.email = email
+    self.user_type = user_type
 
   def __repr__(self):
     return '<User %r>' %self.username
@@ -18,24 +20,26 @@ class Flight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     matricula_aluno = db.Column(db.String())
     date = db.Column(db.Date())
-    dateTimeStart = db.Column(db.DateTime())
-    dateTimeEnd = db.Column(db.DateTime())
+    dateTimeStart = db.Column(db.Date())
+    dateTimeEnd = db.Column(db.Date())
+    grade = db.Column(db.Float())
     comment = db.Column(db.Text())
 
-    def __init__(self, matricula_aluno, date, dateTimeStart, dateTimeEnd, comment):
+    def __init__(self, matricula_aluno, date, dateTimeStart, dateTimeEnd, comment, grade):
         self.matricula_aluno = matricula_aluno
         self.date = date
         self.dateTimeStart = dateTimeStart
         self.dateTimeEnd = dateTimeEnd
         self.comment = comment
+        self.grade = grade
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
 
 class UserSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'username', 'email')
+    fields = ('id', 'username', 'email', 'user_type')
 
 class FlightSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'matricula_aluno', 'date', 'dateTimeStart', 'dateTimeEnd', 'comment')
+    fields = ('id', 'matricula_aluno', 'date', 'dateTimeStart', 'dateTimeEnd', 'comment', 'grade')
